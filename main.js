aa836fe1fead26b63c1ae8e68b4aecef7aebd275
cap-code <advaith007cube@gmail.com>
2020-08-18 18:01:46 +0530

    updating

--- main.js
+++ main.js
@@ -1,23 +1,117 @@
-const shuffleButton=document.querySelector('.shuffle');
+const P2PT = p2pt;
+var p2pt;
+var peers = {};
+var room;
+var lastgot =0;
+var enable = false;
+const shuffleButton = document.querySelector('.shuffle');
 let nameId;
-const canva=document.getElementById("bingo-panel")
-const hostPage=document.querySelector('.host-page');
-const homePage=document.querySelector('.home-page');
-const hostButton=document.getElementById("host-button");
-shuffleButton.style.display="none";
-hostPage.style.display="none";
-canva.style.display="none";
-function host_setup_page(){
-    homePage.style.display="none";
-    hostPage.style.display="";
-    canva.style.display="none";
-    nameId=document.getElementById('name-id').value;
+const namepanel = document.getElementById("idk");
+const canva = document.getElementById("bingo-panel")
+const hostPage = document.querySelector('.host-page');
+const homePage = document.querySelector('.home-page');
+const hostButton = document.getElementById("host-button");
+const loader = document.querySelector('.loading');
+loader.style.display = "none";
+shuffleButton.style.display = "none";
+hostPage.style.display = "none";
+canva.style.display = "none";
+namepanel.style.display="none";
+function host_setup_page() {
+    homePage.style.display = "none";
+    hostPage.style.display = "";
+    canva.style.display = "none";
+    namepanel.style.display="none";
+    nameId = document.getElementById('name-id').value;
 }
-function playArea(){
-    homePage.style.display="none";
-    canva.style.display="";
-    hostPage.style.display="none";
-    shuffleButton.style.display="";
-    nameId=document.getElementById('name-id').value;
-    document.getElementById('globalName').innerHTML=nameId;
+
+function playArea() {
+    homePage.style.display = "none";
+    nameId = document.getElementById('name-id').value;
+    room = document.getElementById("hostroom-id").value;
+    document.getElementById('globalName').innerHTML = nameId;
+    if (room == "") {
+        alert("please type a room id");
+    } else {
+        // enable = true;
+        // canva.style.display="";
+        loader.style.display = "";
+        hostPage.style.display = "none";
+        shuffleButton.style.display = "";
+        init();
+        listen();
+    }
 }
+
+function playjoin() {
+    homePage.style.display = "none";
+    nameId = document.getElementById('name-id').value;
+    room = document.getElementById("join-is").value;
+    document.getElementById('globalName').innerHTML = nameId;
+    if (room == "") {
+        alert("please type a room id");
+    } else {
+        loader.style.display = "";
+        hostPage.style.display = "none";
+        shuffleButton.style.display = "";
+        init();
+        listen();
+    }
+
+}
+
+function init() {
+    var announceURLs = [
+        "wss://tracker.openwebtorrent.com",
+        "wss://tracker.sloppyta.co:443/announce",
+        "wss://tracker.novage.com.ua:443/announce",
+        "wss://tracker.btorrent.xyz:443/announce"
+    ];
+    p2pt = new P2PT(announceURLs, room);
+    p2pt.on('trackerconnect', (tracker, stats) => {
+        console.log("connected to tracker :", tracker);
+        console.log("status :", JSON.stringify(stats));
+        loader.style.display = "none";
+        enable = true;
+        canva.style.display = "";
+        namepanel.style.display="";
+
+    });
+}
+
+function listen() {
+    p2pt.on('peerconnect', peer => {
+        peers[peer.id] = peer;
+        console.log("new peer connnected :", peer);
+        console.log("secrest inside the peers :",peers);
+        document.getElementById("players").innerHTML="number of players :" +Object.keys(peers).length;
+    });
+    p2pt.on('peerclose', peer => {
+        console.log("peer disconnnected :", peer.id);
+        delete peers[peer.id];
+        document.getElementById("players").innerHTML="number of players :" +Object.keys(peers).length;
+
+    });
+    //get msg from client
+    p2pt.on('msg', (peer, msg) => {
+        var text = JSON.parse(msg);
+        console.log(text);
+        if (text.type == "play") {
+            lastgot = text.bvalue;
+            document.getElementById("emitted-value").innerHTML = text.name + " : " + text.bvalue;
+        } else if (text.type == "won") {
+            document.getElementById("won").innerHTML = text.nameId + " won ";
+            canva.style.display = "none";
+        }
+    });
+    p2pt.start();
+}
+
+function broadcast(peers, data) {
+    console.log("data sent to peer :", data);
+    for (var key in peers) {
+        console.log("key: ",key);
+        p2pt.send(peers[key], JSON.stringify(data));
+    }
+}
+
--- sketch.js
+++ sketch.js
@@ -1,166 +1,194 @@
 let ctx;
-let cols=5;
-let rows=5;
-let p=[];
+let cols = 5;
+let rows = 5;
+let p = [];
 let shuffle;
 let bingo;
-let cellValue;
-let totalValue=0;
+let totalValue = 0;
+let ontime;
+
 function setup() {
-	ctx=createCanvas(301, 301);
+	ctx = createCanvas(301, 301);
 	ctx.parent('bingo-panel');
-	var c=1;
-		for(let i=0;i<rows;i++){
-		p[i]=[];
-       for(let j=0;j<cols;j++){
-		  p[i][j]= new grid(i*60,j*60,c);
-		  c++;
-		  
-	   }
+	var c = 1;
+	for (let i = 0; i < rows; i++) {
+		p[i] = [];
+		for (let j = 0; j < cols; j++) {
+			p[i][j] = new grid(i * 60, j * 60, c);
+			c++;
+
+		}
 
 	}
-	shuffle=select('#shuffleButton','.shuffle');
+	shuffle = select('#shuffleButton', '.shuffle');
 	shuffle.mouseClicked(shuffling);
-	bingo=select('#bingo-tracking','.bingo-panel');
+	bingo = select('#bingo-tracking', '.bingo-panel');
+	ontime = true;
 }
-function shuffling(){
-	for(let i=0;i<rows;i++){
-		for(let j=0;j<cols;j++){
-			let k=floor(random(0,4));
-			let l=floor(random(0,4));
-			let temp=p[i][j].v;
-			p[i][j].v=p[k][l].v;
-			p[k][l].v=temp;
+
+function shuffling() {
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			let k = floor(random(0, 4));
+			let l = floor(random(0, 4));
+			let temp = p[i][j].v;
+			p[i][j].v = p[k][l].v;
+			p[k][l].v = temp;
 		}
 	}
-shuffle.style('display','none');	
+	shuffle.style('display', 'none');
 }
 
 function draw() {
-background(255);
-totalValue=0;
-let d=0;
-for(let i=0;i<rows;i++){
-	for(let j=0;j<cols;j++){
-		if(i==j){
-			if(p[i][j].ticked==true){
-				d++;
-				
-				//console.log(d);
+
+	background(255);
+	totalValue = 0;
+	let d = 0;
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			if (i == j) {
+				if (p[i][j].ticked == true) {
+					d++;
+
+					//console.log(d);
+				}
 			}
-	    }
-  
-	}
-	if(d==5){
-		totalValue++;
-		d=0;
-	}
-}
-for(let i=0;i<rows;i++){
-	let c=0;
-	for(let j=0;j<cols;j++){
-		p[i][j].show();
-		p[i][j].cellHover();
-		if(p[i][j].ticked==true){
-			c++;
-			//console.log("c register");
+
 		}
-		
-	}
-	   
-        if(c==5){
+		if (d == 5) {
 			totalValue++;
-			c=0;
+			d = 0;
+		}
+	}
+	for (let i = 0; i < rows; i++) {
+		let c = 0;
+		for (let j = 0; j < cols; j++) {
+			p[i][j].show();
+			p[i][j].cellHover();
+			if (p[i][j].ticked == true) {
+				c++;
+				//console.log("c register");
+			}
+
+		}
+
+		if (c == 5) {
+			totalValue++;
+			c = 0;
 			//console.log("cols and dia");
 		}
-	}	
+	}
 
-for(let i=0;i<rows;i++){
-	let r=0;
-	for(let j=0;j<cols;j++){
-		//p[j][i].show();
-		p[j][i].cellHover();
-		if(p[j][i].ticked==true){
-			r++;
-			//console.log("r registered");
-		}
-        if(r==5){
-			totalValue++;
-			r=0;
-			//console.log("row");
-		}
-	}	
-}
-let dr=0;
-for(let i=0;i<rows;i++){
-	for(let j=0;j<cols;j++){
-		//p[j][i].show();
-		p[i][j].cellHover();
-		  if(i+j==4){
-			if(p[i][j].ticked==true){
-				dr++;
-				//console.log(dr);
+	for (let i = 0; i < rows; i++) {
+		let r = 0;
+		for (let j = 0; j < cols; j++) {
+			//p[j][i].show();
+			p[j][i].cellHover();
+			if (p[j][i].ticked == true) {
+				r++;
+				//console.log("r registered");
+			}
+			if (r == 5) {
+				totalValue++;
+				r = 0;
+				//console.log("row");
 			}
 		}
-        if(dr==5){
-			totalValue+=1;
-			//console.log("sec-dia");
-			dr=0;
-		}
-	}	
-}
-//console.log("totalValue:",totalValue);
-if(totalValue==1){
-	//console.log("b");
-	bingo.html('B');
-}else if(totalValue==2){
-	bingo.html('BI');
-}else if(totalValue==3){
-	bingo.html('BIN');
-}else if(totalValue==4){
-	bingo.html('BING');
-}else if(totalValue==5){
-	bingo.html('BINGO');
-}
-}
-class grid{
-	constructor(x,y,v){
-		this.x=x;
-		this.y=y;
-		this.v=v;
-		this.hover=false;
-		this.ticked=false;
 	}
-	show(){
-		if(this.hover==false){
+	let dr = 0;
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			//p[j][i].show();
+			p[i][j].cellHover();
+			if (i + j == 4) {
+				if (p[i][j].ticked == true) {
+					dr++;
+					//console.log(dr);
+				}
+			}
+			if (dr == 5) {
+				totalValue += 1;
+				//console.log("sec-dia");
+				dr = 0;
+			}
+		}
+	}
+	//console.log("totalValue:",totalValue);
+	if (totalValue == 1) {
+		//console.log("b");
+		bingo.html('B');
+	} else if (totalValue == 2) {
+		bingo.html('BI');
+	} else if (totalValue == 3) {
+		bingo.html('BIN');
+	} else if (totalValue == 4) {
+		bingo.html('BING');
+	} else if (totalValue == 5) {
+		bingo.html('BINGO');
+		if (ontime == true) {
+			var type = "won";
+			var name = {
+				type,
+				nameId
+			}
+			broadcast(peers, name);
+			ontime = false;
+		}
+	}
+}
+class grid {
+	constructor(x, y, v) {
+		this.x = x;
+		this.y = y;
+		this.v = v;
+		this.hover = false;
+		this.ticked = false;
+		this.again = true;
+	}
+	show() {
+		if (this.hover == false) {
 			fill(255);
 
-		}else{
-			fill(0,255,0,50);
+		} else {
+			fill(0, 255, 0, 50);
 		}
-		rect(this.x,this.y,60,60,10);
+		rect(this.x, this.y, 60, 60, 10);
 		fill(0);
-		textAlign(CENTER,CENTER);
+		textAlign(CENTER, CENTER);
 		textSize(23);
-		text(this.v,this.x+30,this.y+30);
+		text(this.v, this.x + 30, this.y + 30);
 	}
-	cellHover(){
-		if(mouseIsPressed){
-		if(mouseX>this.x&&mouseX<this.x+60&&mouseY>this.y&&mouseY<this.y+60)
-			{
-				this.hover=true;
-				if(mouseIsPressed){
-				this.ticked=true;
-			
-			}
-			
-		}else{
-			this.hover=false;
-		}
-	  }
-	if(this.ticked==true){
-		line(this.x+3,this.y+3,this.x+60-3,this.y+60-3);
-	}
+	cellHover() {
+		if (enable) {
+			if (mouseIsPressed) {
+				//let onesend=true;
+				if (mouseX > this.x && mouseX < this.x + 60 && mouseY > this.y && mouseY < this.y + 60) {
+					this.hover = true;
+					if (mouseIsPressed) {
+						this.ticked = true;
+						if (this.again == true) {
+							var type = "play"
+							var name = nameId;
+							if (this.v != lastgot) {
+								var bvalue = this.v;
+								var data = {
+									type,
+									name,
+									bvalue
+								};
+								broadcast(peers, data);
+								this.again = false;
+							}
+						}
+					}
 
+				} else {
+					this.hover = false;
+				}
+			}
+			if (this.ticked == true) {
+				line(this.x + 3, this.y + 3, this.x + 60 - 3, this.y + 60 - 3);
+			}
+		}
 	}
 }
